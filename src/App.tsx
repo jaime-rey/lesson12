95e70ad3ee974ee164ef75e0831d609cae28f113
jaime-rey <jaimereycasado@gmail.com>
2023-07-12 00:13:18 +0200

    lesson12

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-
 import './App.css'
 import Heading from './components/Heading'
 import { Section } from './components/Section'
 import Counter from './components/Counter'
+import List from './components/List'
 
 function App() {
   
@@ -11,6 +11,7 @@ function App() {
   <Heading title={'Hello Heading'} />
   <Section title='NEW SECTION'><h3>This is my section</h3></Section>
   <Counter />
+  <List items={["coffee", "tacos", "code"]} render = {(item: string)=><span className='gold'>{item}</span>} />
   </>
   )
 }
